cca154f8c78171f212c671983a755f83ef7c9b14
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2023-03-26 03:40:10 +0600

    EMAIL PASSWORD SIGN UP  comopleted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import app from './firebase/firebase.init';
 import { useState } from 'react';
 
@@ -7,66 +7,94 @@ const auth = getAuth(app)
 
 function App() {
   const [user, setUser] = useState({})
+  const [success, setSuccess] = useState(false)
 
-  const googleProvider =new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
   const githubProvider = new GithubAuthProvider();
 
-  const handleGoogleSignIn = () =>{
+  const handleGoogleSignIn = () => {
     signInWithPopup(auth, googleProvider)
-    .then( result=>{
-      const user = result.user;
-      setUser(user);
-      console.log(user)
-    })
-    .catch(error=>{
-      console.error('error: ', error)
-    })
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        console.log(user)
+      })
+      .catch(error => {
+        console.error('error: ', error)
+      })
   }
   const handleGithubSignIn = () => {
-    signInWithPopup(auth , githubProvider)
-    .then(result=>{
-      const user = result.user;
-      setUser(user);
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch(error => {
+        console.error('error: ', error)
+      })
+  }
+
+  const handleRegister = (event) => {
+     event.preventDefault();
+     setSuccess(false)
+     const form = event.target;
+     const email = form.email.value;
+     const password = form.password.value;
+     createUserWithEmailAndPassword(auth,email , password)
+     .then((userCredential) => {
+      const user = userCredential.user;
+      setSuccess(true)
+      form.reset();
+      
     })
-    .catch(error=>{
+    .catch(error => {
       console.error('error: ', error)
     })
   }
 
-  const handleSignOut = () =>{
+  const handleSignOut = () => {
     signOut(auth)
-    .then(()=>{
-      setUser({});
-    })
-    .catch(()=>{
-      setUser({});
-    })
+      .then(() => {
+        setUser({});
+      })
+      .catch(() => {
+        setUser({});
+      })
   }
   return (
     <div className="App">
 
       {
         user.uid ?
-        <button onClick={()=>handleSignOut()}>SignOut</button> 
-        :
-        <>
-         <button onClick={()=>handleGoogleSignIn()}>Google SignIn</button>
-         <button onClick={()=>handleGithubSignIn()}>Github SignIn</button>
-        </>
-       
+          <button onClick={() => handleSignOut()}>SignOut</button>
+          :
+          <>
+            <button onClick={() => handleGoogleSignIn()}>Google SignIn</button>
+            <button onClick={() => handleGithubSignIn()}>Github SignIn</button> 
+
+            <form onSubmit={handleRegister}>
+              <input type="email" name="email" placeholder='Enter email ' />
+              <br />
+              <input type="password" name="password" placeholder='enter password' />
+              <br />
+              <button type="submit">Submit</button>
+
+            </form>
+          </>
+
       }
-     
-      
-      
-        {
-          user.uid &&
-          <div>
+
+
+
+      {
+        user.uid &&
+        <div>
           <h2>User name: {user.displayName}</h2>
           <h2>User Email: {user.email}</h2>
           <img src={user.photoURL} alt="" />
         </div>
-        }
-       
+      }
+
 
     </div>
   );
